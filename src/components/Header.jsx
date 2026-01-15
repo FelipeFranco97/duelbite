271b06acba91328949e3873e90175798933ec38b
Felipe Franco <francozuluaga97@gmail.com>
2026-01-15 17:53:07 -0500

    Fix Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src="/img/logo.jpeg" alt="Logo" className={styles.logo} />
+      <img src="/img/logo.png" alt="Logo" className={styles.logo} />
     </header>
   );
 }
