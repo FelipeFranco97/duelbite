629a67ad31635f9742c84e40c7508fd0317b4c1b
Felipe Franco <francozuluaga97@gmail.com>
2026-01-19 21:27:46 -0500

    Header Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,16 @@ import styles from '../styles/Header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src="/img/logo.png" alt="Logo" className={styles.logo} />
-    </header>
+  <div className={styles.leftLogoGroup}>
+    <img src="/img/BW Logo.png" alt="BW Logo" className={styles.logoMark} />
+    <img src="/img/Letras_black_Widow.png" alt="Black Widow" className={styles.logoText} />
+  </div>
+
+  <img src="/img/logo.png" alt="Logo" className={styles.mainLogo} />
+
+  <img src="/img/Logo definitivo 3.png" alt="Logo definitivo" className={styles.rightLogo} />
+</header>
   );
 }
 
-export default Header
+export default Header;
